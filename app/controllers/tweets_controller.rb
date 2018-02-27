f8acc43d141c6bc6bf204c56dfae3b236cbbcc40
kazuki <neworder1988@i.spftbank.jp>
2018-02-27 23:22:09 +0900

    first commit TweetClone

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,57 @@
+class TweetsController < ApplicationController
+  before_action :set_params, only:[:show, :edit, :destory, :update]
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    if params[:back]
+      @tweet = Tweet.new(tweet_params)
+    else
+      @tweet = Tweet.new
+    end
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: "ツイートを投稿しました"
+    else
+      render "new"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "ツイートを編集しました"
+    else
+      render "edit"
+    end
+  end
+
+  def confirm
+    @tweet = Tweet.new(tweet_params)
+  end
+
+  def destroy
+    @tweet.destory
+    redirect_to tweet_path, notice: "ツイートを削除しました"
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
+
+  def set_params
+    @tweet = Tweet.find(params[:id])
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+root to: 'tops#index'
+
+resources :tweets do
+  collection do
+    post :confirm
+  end
+end
+
+end
